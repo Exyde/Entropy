8325f14f4f3414cc72815809302ec35d2411c809
Younes <younesskate@hotmail.fr>
2024-05-07 13:57:23 +0200

    Project Setup, Game Window Setup.

---
+++ Application/ColorUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using OpenTK.Mathematics;
+
+namespace Entropy.Application;
+
+public static class ColorUtility
+{
+    public static readonly Color4<Rgba> Black = new Color4<Rgba>(0, 0, 0, 1);
+    public static readonly Color4<Rgba> White = new Color4<Rgba>(1, 1, 1, 1);
+    public static readonly Color4<Rgba> Red = new Color4<Rgba>(1, 0, 0, 1);
+
+}
---
+++ Application/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Drawing;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+//https://opentk.net/learn/chapter1/2-hello-triangle.html?tabs=onload-opentk4%2Conrender-opentk4%2Cresize-opentk4
+
+namespace Entropy.Application
+{
+    public static class Application
+    {
+        static void Main(string[] args)
+        {
+            using Game game = new Game(800 ,600,  "Entropy Engine");
+            game.Run();
+        }
+    }
+    
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) :
+            base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title})
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Called once on window first open
+        /// </summary>
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            
+            GL.ClearColor(ColorUtility.Red);
+        }
+        
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+            
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+    }
+}
